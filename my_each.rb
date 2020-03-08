e1f547933839c9dc6b5a02a393ea9f538fd27064
Amal Abdu <abduamal@ymail.com>
2020-03-08 06:25:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,7 @@ def my_each(array)
   i = 0 
   
   while collecter.length < array.length
-    yield array[i] { |name| name + "What's up"}
+    yield array[i] { |name| name + ", what's up"}
     i = i + 1
   end
   
