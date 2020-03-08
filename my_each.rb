d7aa9609c9f402bc82480ff2503fe071aaeaebbc
Amal Abdu <abduamal@ymail.com>
2020-03-08 06:03:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  collecter = []
+  
+  while collecter.length < array.length
+    yield array(collecter) do |word|
+      collecter << word
+    end
+  end
+  
+end
+
+array = [joe, flo, moe]
+
+yielding do |word|
+  collecter << word
 end
